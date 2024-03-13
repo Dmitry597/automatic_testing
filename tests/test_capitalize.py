ed5febf714bac33310215899370ef04d5afbc774
Dmitry Grigoriev <diman03051995@mail.ru>
2024-03-13 11:32:30 +0300

    modifide test_capitalize

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -2,10 +2,7 @@
 from package_name.capitalize import capitalize
 
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все тесты пройдены!')
